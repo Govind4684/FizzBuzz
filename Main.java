6ce3c97d15c0b48bd4961ca6fd82c990d1734ebb
Govind4684 <kumargovindkarn@gmail.com>
2021-06-08 02:39:24 +0530

    Final Commit

--- Main.java
+++ Main.java
@@ -3,7 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String text = scan.nextLine();
-        System.out.println(text);
+        System.out.print("Number: ");
+        int number = scan.nextInt();
+
+        if ((number % 5 == 0) && (number % 3 == 0))
+            System.out.println("FizzBuzz");
+        else if (number % 5 == 0)
+            System.out.println("Fizz");
+        else if (number % 3 == 0)
+            System.out.println("Buzz");
+        else
+            System.out.println(number);
+
+        scan.close();
     }
 }
